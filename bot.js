7485456cd666aaef4eccea9bde34f68715c54972
requiesce <31718966+requiesce@users.noreply.github.com>
2018-01-21 07:06:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,33 @@ client.on('message', message => {
 });
 
 
+const commandPrefix = '!';
 
+var commands = [
+	{
+		command: "summon",
+		description: "Summons me to your voice channel",
+		parameters: [],
+		execute: function(message, params) {
+			if (message.member.voiceChannel) {
+				summon(message.member.voiceChannel);
+			} else {
+				message.reply("You should join a voice channel first.");
+			}
+		}
+	}
+];
+
+function summon(voiceChannel)
+{
+	voiceChannel.join().then(connection => {voice_connection = connection;}).catch(console.error);
+	setTimeout(function () {
+		if (!voiceChannel.members.get(bot.user.id))
+		{
+			restart();
+		}			
+	}, 5000);
+}
 
 
 // THIS  MUST  BE  THIS  WAY
